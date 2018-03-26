08787f18c34fffe7643a5f16fcb1683ea6a8a8f7
nramnad <nramnad@hotmail.com>
2018-03-26 22:08:35 +0530

    fix checkstyle warings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,19 +1,23 @@
 package com.example;
 
 /**
- * This is a class.
- */
+ *  * This is a class.
+ *   */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+	  /**
+	   *    * This is a constructor.
+	   *       */
+	  public Greeter() {
 
-  }
+		    }
+	    
+	    /**
+	     *     * @param someone the name of a person
+	     *         * @return greeting string
+	     *           */
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+	     public final String greet(final String someone) {
+		         return String.format("Hello, %s!", someone);
+			   }
 }
