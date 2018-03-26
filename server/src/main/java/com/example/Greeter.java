e9ebc3498eda227bec124a59220289020e98cd72
nramnad <nramnad@hotmail.com>
2018-03-26 22:36:39 +0530

    for whitespace

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,23 +1,20 @@
 package com.example;
 
 /**
- *  * This is a class.
- *   */
+ ** This is a class.
+ **/
+
 public class Greeter {
-
-	  /**
-	   *    * This is a constructor.
-	   *       */
-	  public Greeter() {
-
-		    }
-	    
-	    /**
-	     *     * @param someone the name of a person
-	     *         * @return greeting string
-	     *           */
-
-	     public final String greet(final String someone) {
-		         return String.format("Hello, %s!", someone);
-			   }
+	/**
+	 * ** This is a constructor
+	 * **/
+	public Greeter() {
+			 }	    
+	/**
+	 * ** @param someone the name of a person
+	 * ** @return greeting string
+	 * **/
+	public final String greet(final String someone) {
+		return String.format("Hello, %s!", someone);
+	}
 }
